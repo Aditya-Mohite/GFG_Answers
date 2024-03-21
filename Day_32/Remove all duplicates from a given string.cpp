ae4010f5dda093765a65d9647cbdfca76b0d322e
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-03-21 19:57:17 +0530

    Create Remove all duplicates from a given string.cpp

---
+++ Day_32/Remove all duplicates from a given string.cpp
@@ -0,0 +1,19 @@
+
+class Solution
+{
+public:
+	string removeDuplicates(string str) 
+	{
+	    string ans;
+	    unordered_map<char, int> mp;
+	    for(int i=0; i<str.size(); i++)
+	    {
+	        mp[str[i]]++;
+	        if(mp[str[i]] == 1)
+	        {
+	            ans += str[i];
+	        }
+	    }
+	    return ans;
+	}
+};
