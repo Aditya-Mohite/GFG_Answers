e184ca96ca65714a63b6db3e44dd62077d3fc6e4
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-04-03 22:45:49 +0530

    Create Anagram.cpp

---
+++ Day_45/Anagram.cpp
@@ -0,0 +1,19 @@
+
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b)
+    {
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        if(a == b)
+        {
+            return true;
+        }
+        else
+        return false;
+        
+    }
+
+};
