c4a5fb44b12ffd4259ca9e1502505a175b741407
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-04-09 22:39:51 +0530

    Create Square root of a number.cpp

---
+++ Day_51/Square root of a number.cpp
@@ -0,0 +1,10 @@
+
+// Function to find square root
+// x: element to find square root
+class Solution{
+  public:
+    long long int floorSqrt(long long int x) 
+    {
+        return floor(sqrt(x));
+    }
+};
