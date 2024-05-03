000f81600838d34087b4c448514fe9fb7ca0e7db
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-05-03 22:24:30 +0530

    Create Arithmetic Number.cpp

---
+++ Day_75/Arithmetic Number.cpp
@@ -0,0 +1,17 @@
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C)
+    {
+        if(A==B && C==0)
+        {
+            return 1;
+        }
+        double n = (double)(B-A)/C+1;
+        if(n != int(n))
+        {
+            return 0;
+        }
+        return n>0 ? 1 : 0;
+    }
+};
