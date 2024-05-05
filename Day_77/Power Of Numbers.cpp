78ca64e041cc711429d48218838a8eca35341bdf
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-05-05 23:04:28 +0530

    Create Power Of Numbers.cpp

---
+++ Day_77/Power Of Numbers.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+    //You need to complete this fucntion
+    int mod  = 1000000007;
+    long long power(int N,int R)
+    {
+       if(R==1) return N;
+       if(R==0) return 1;
+      
+       long long val=power(N,R/2)%mod;
+       val=(val*val)%mod;
+      
+       if(R&1) val=(val*N)%mod;
+      
+       return val; 
+        
+    }
+
+};
