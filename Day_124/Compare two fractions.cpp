996cea7e14d4c49a6302193f5d870fbaf23150eb
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-06-21 22:33:45 +0530

    Create Compare two fractions.cpp

---
+++ Day_124/Compare two fractions.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+  public:
+    string compareFrac(string str) 
+    {
+        int a, b, c, d;
+        stringstream ss(str);
+        char slash, comma;
+        ss >> a >> slash >> b >> comma;
+        ss >> c >> slash >> d;
+        int left = a * d;
+        int right = b * c;
+        if (left > right) 
+        {
+            return to_string(a) + "/" + to_string(b);
+        } 
+        else if (left < right) 
+        {
+            return to_string(c) + "/" + to_string(d);
+        } 
+        else 
+        {
+            return "equal";
+        }
+    }
+};
