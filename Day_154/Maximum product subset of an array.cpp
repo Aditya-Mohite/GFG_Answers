4659980ebc2e848133e325e75af6345dd7deab62
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-07-21 19:52:12 +0530

    Create Maximum product subset of an array.cpp

---
+++ Day_154/Maximum product subset of an array.cpp
@@ -0,0 +1,39 @@
+
+class Solution 
+{
+  public:
+    long long int findMaxProduct(vector<int>& arr) 
+    {
+        if(arr.size() == 1) 
+            return arr[0];
+        long long mod = 1e9+7;
+        int flag = 0;
+        long long prod = 1, mini = INT_MIN;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i]==0)
+            {
+                flag = 1;
+                continue;
+            }
+            if(arr[i]>0)
+            {
+                prod = ((prod)%mod * arr[i])%mod;
+            }
+            else if(arr[i]<0)
+            {
+                prod = ((prod)%mod * arr[i])%mod;
+                mini = max((long long)arr[i],mini);
+            }
+        }
+        if(prod<0)
+        {
+            prod = prod/mini;
+        }
+        if(prod==1 && flag==1) 
+            return 0;
+            
+        return prod;
+    }
+};
+
