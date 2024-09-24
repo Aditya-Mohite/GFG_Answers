eff1a54964732edceea72e0a51b3de1c3e2e72fd
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-09-24 22:49:13 +0530

    Create Smallest window in a string containing all the characters of another string.cpp

---
+++ Day_218/Smallest window in a string containing all the characters of another string.cpp
@@ -0,0 +1,40 @@
+
+class Solution
+{
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    string smallestWindow (string str, string p)
+    {
+        int arr[26]={0};
+        int siz = p.length(), j=0, n=str.length();
+        int brr[26]={0}, mini=INT_MAX, idx=-1;
+        char c;
+        for(auto i:p) arr[i-'a']++;
+        for(int i=0; i<n; i++)
+        {
+            c = str[i];
+            if(arr[c-'a']>=1&&brr[c-'a']<arr[c-'a'])
+                siz--;
+            brr[c-'a']++;
+            while(siz==0)
+            {
+                if(mini>(1+i-j))
+                {
+                    mini=1+i-j;
+                    idx=i;
+                }
+                c=str[j];
+                j++;
+                brr[c-'a']--;
+                if(arr[c-'a']>brr[c-'a'])
+                    siz++;
+            }
+            
+        }
+        if(mini==INT_MAX)
+            return "-1";
+            
+        return str.substr(1+idx-mini,mini);
+    }
+};
