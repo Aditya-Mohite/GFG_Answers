ae6d60e483e544a23e585320345e1527829b6d52
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-10-02 23:13:16 +0530

    Create Rotate and delete.cpp

---
+++ Day_226/Rotate and delete.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) 
+    {
+        int n = arr.size();
+
+        if (n == 1) {
+            return arr[0];
+        } 
+        else 
+        {
+            if (n % 2 == 0) 
+            {
+                return arr[2 + (n - 2) / 4 - 1];
+            } 
+            else 
+            {
+                return arr[3 + (n - 3) / 4 - 1];
+            }
+        }
+    }
+};
