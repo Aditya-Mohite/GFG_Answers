24407830aea54a496738e4aabb548dc5ede5b57f
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-10-11 22:55:14 +0530

    Create Reorganize The Array.cpp

---
+++ Day_235/Reorganize The Array.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) 
+    {
+        vector<int> ans(arr.size(), -1);
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i] >= 0)
+            {
+                ans[arr[i]] = arr[i];
+            }
+        }
+        return ans;
+    }
+};
