803477865b354c67d5a2fbd9160709240c1f5285
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-10-15 16:49:45 +0530

    Create Subarray range with given sum.cpp

---
+++ Day_239/Subarray range with given sum.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) 
+    {
+        unordered_map<int, int> mp;
+        int cnt = 0, sum = 0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            sum += arr[i];
+            if(sum == tar)
+                cnt++;
+                
+            if(mp.find(sum - tar) != mp.end())
+            {
+                cnt += mp[sum - tar];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
