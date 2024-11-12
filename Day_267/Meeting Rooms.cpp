d34c82751863dba77f4bf25dddb31fd9daaae268
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-11-12 22:52:15 +0530

    Create Meeting Rooms.cpp

---
+++ Day_267/Meeting Rooms.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) 
+    {
+        sort(arr.begin(), arr.end());
+        for (int i = 1; i < arr.size(); i++)
+        {
+            int end = arr[i - 1][1];
+            int start = arr[i][0];
+            if (start < end)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
