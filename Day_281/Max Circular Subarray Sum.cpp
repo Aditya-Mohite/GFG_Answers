cc202f41c8e10a3eed1a952d84027058d0821a91
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-11-26 23:27:36 +0530

    Create Max Circular Subarray Sum.cpp

---
+++ Day_281/Max Circular Subarray Sum.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+  public:
+    // arr: input array
+    // Function to find maximum circular subarray sum.
+    int circularSubarraySum(vector<int> &arr) 
+    {
+        int s=0;
+        int sum=0;
+        int res=0;
+        for(auto it:arr)
+        {
+            sum+=it;
+            s+=it;
+            if(s<0)
+            s=0;
+            
+            res=max(res,s);
+        }
+        s=0;
+        int x = INT_MAX;
+        for(auto it:arr)
+        {
+            s+=it;
+            if(s>0)
+            s=0;
+            
+            x=min(x,s);
+        }
+        res = max(res,sum-x);
+        return res;
+    }
+};
