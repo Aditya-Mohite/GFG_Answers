a2b8df9e985dd18012aa0d7b8bc0c0a375b9becb
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-12-06 22:25:10 +0530

    Create Find H-Index.cpp

---
+++ Day_290/Find H-Index.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    // Function to find hIndex
+    int hIndex(vector<int>& citations) 
+    {
+        sort(citations.begin(), citations.end()) ;
+        int n = citations.size();
+        int ans = 0 ;
+        for(int i = n - 1; i >= 0; i --)
+        {
+            ans = max(ans , min(citations[i], n - i)) ;
+        }
+        return ans;
+    }
+};
