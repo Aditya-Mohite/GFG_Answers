9218066092ef740353e6ce3d93ff6959e97859ea
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2024-12-12 23:19:04 +0530

    Create Number of occurrence.cpp

---
+++ Day_296/Number of occurrence.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    int countFreq(vector<int>& arr, int target) 
+    {
+        int cnt = 0;
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i] == target)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
